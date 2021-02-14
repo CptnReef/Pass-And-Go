0077ffa65f2d3c51fc3774e7e868df0a5d93fb2a
Jonathan Warner <jewarner57@gmail.com>
2021-02-14 17:57:14 -0500

    feat: improve form, style auth templates

--- pass_go/Pass_Go/blueprints/user/forms.py
+++ pass_go/Pass_Go/blueprints/user/forms.py
@@ -1,30 +1,31 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, ValidationError
+from wtforms.validators import DataRequired, Length, Email, ValidationError
 # from Pass_Go.models import User
 
 
 class SignUpForm(FlaskForm):
     """Form for creating a new user"""
-    username = StringField('User Name',
-                           validators=[DataRequired(),
-                                       Length(min=3, max=50)])
+
+    username = StringField('User Name', validators=[DataRequired(), Length(min=3, max=50)])
+
+    email = StringField("Email", validators=[DataRequired(), Length(min=3, max=80), Email()])
+
     password = PasswordField('Password', validators=[DataRequired()])
+
     submit = SubmitField('Sign Up')
 
     # need User model from db for this to work
     # --
-    # def validate_username(self, username):
-    #     user = User.query.filter_by(username=username.data).first()
+    # def validate_email(self, email):
+    #     user = User.query.filter_by(email=email.data).first()
     #     if user:
     #         raise ValidationError(
-    #             'That username is taken. Please choose a different one.')
+    #             'That email is already in use by a different account.')
 
 
 class LoginForm(FlaskForm):
     """Form for logging in a user"""
-    username = StringField('User Name',
-                           validators=[DataRequired(),
-                                       Length(min=3, max=50)])
+    email = StringField("Email", validators=[DataRequired(), Length(min=3, max=80), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Log In')
